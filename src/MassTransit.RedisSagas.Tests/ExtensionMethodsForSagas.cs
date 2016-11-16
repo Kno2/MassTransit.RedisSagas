fb792b02523a656f746d00a2f2f92cb3c0aa858d
Ryan Kelley <ryan.kelley@royaljay.net>
2016-11-16 10:40:41 -0700

    mendTests in place, failing

---
+++ src/MassTransit.RedisSagas.Tests/ExtensionMethodsForSagas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit.Saga;
+
+namespace MassTransit.RedisSagas.Tests
+{
+    public static class ExtensionMethodsForSagas
+    {
+        public static bool ShouldContainSaga<TSaga>(this ISagaRepository<TSaga> repository, Guid sagaId, TimeSpan timeout)
+           where TSaga : class, ISaga
+        {
+            DateTime giveUpAt = DateTime.Now + timeout;
+
+            while (DateTime.Now < giveUpAt)
+            {
+                var saga = (repository as IRetrieveSagaFromRepository<TSaga>).GetSaga(sagaId);
+                if (saga != null) return true;
+                Task.Delay(10);
+            }
+
+            return false;
+        }
+
+        public static async Task<bool> ShouldContainSaga<TSaga>(this ISagaRepository<TSaga> repository, Guid sagaId, Func<TSaga, bool> condition, TimeSpan timeout)
+           where TSaga : class, ISaga
+        {
+            DateTime giveUpAt = DateTime.Now + timeout;
+
+            while (DateTime.Now < giveUpAt)
+            {
+                var saga = (repository as IRetrieveSagaFromRepository<TSaga>).GetSaga(sagaId);
+                if (condition(saga)) return true;
+                await Task.Delay(10);
+            }
+
+            return false;
+        }
+    }
+}
